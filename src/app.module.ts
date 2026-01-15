61fe683d096a0d93f52a9da388b2406e1e65c624
rosifloresr <floresmrosi@gmail.com>
2026-01-15 17:40:18 -0300

    Arrancando challenge NestJS con PostgreSQL y TypeORM

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type:'postgres',
+        host: config.get('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities:true,
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
